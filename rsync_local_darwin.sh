561c281fe822b7f0769c6f728733480753e25df4
alfredo-milani <alfredo.milani.94@gmail.com>
2018-12-03 22:42:15 +0100

    Update rsync_local_darwin.sh

--- rsync_local_darwin.sh
+++ rsync_local_darwin.sh
@@ -20,12 +20,21 @@ declare -r BD='\e[1m' # Bold
 declare -r NC='\033[0m' # No Color
 
 # Readonly vars
+declare -r -i MIN_BASH_V=4
+declare -r -i BASH_V=${BASH_VERSION:0:1}
 declare -r -i EXIT_SUCCESS=0
 declare -r -i EXIT_FAILURE=1
 declare -r -i EXIT_MISSING_BACKUP_FILENAMES=2
 declare -r DEV_NULL='/dev/null'
 declare -r NULL='null'
 declare -r script_name="`basename "$0"`"
+
+if [ ${BASH_V} -lt ${MIN_BASH_V} ]; then
+	echo "ERRORE: Version bash obsoleta."
+	echo "Versione correte: ${BASH_V}"
+	echo "Minima versione richiesta: ${MIN_BASH_V}"
+	exit ${EXIT_FAILURE}
+fi
 
 declare tmp_dir='/tmp'
 declare log_file="${NULL}"
@@ -37,27 +46,6 @@ declare print_conf=false
 declare ask=true
 declare log_on_file=false
 
-
-
-function read_filename_to_backup {
-	if ! [ -s "${config_file}" ]; then
-		msg 'R' "ERRORE: Il file \"${config_file}\" sembra non contenere informazioni utili."
-		return ${EXIT_FAILURE}
-    elif [ -f "${config_file}" ]; then
-        while IFS='' read -r path; do
-        	# bug: se legge riga del tipo ":/Volumes" considera chiave:"Volumes", value:"null"
-        	# invece dovrebbe considerare il contrario
-        	tmp=(${path//:/ })
-        	[ -z "${tmp[0]}" ] && tmp[0]="${NULL}"
-        	[ -z "${tmp[1]}" ] && tmp[1]="${NULL}"
-
-    		source_dest_backup+=(["${tmp[0]}"]="${tmp[1]}")
-		done <<< `sed -e 's/[[:space:]]*\[.*]//; s/[[:space:]]*#.*//; /^[[:space:]]*$/d;' "${config_file}"`
-    else
-        msg 'R' "ERRORE: il file utilizzato per l'inizializzazione non è stato trovato! (PATH: ${config_file})"
-        return ${EXIT_FAILURE}
-    fi
-}
 
 function msg {
 	case "${1}" in
@@ -71,14 +59,54 @@ function msg {
 	esac
 }
 
+function check_tools {
+	local tools_missing=false
+
+	while [ ${#} -gt 0 ]; do
+		command -v "${1}" &> ${DEV_NULL}
+		if [ ${?} != 0 ]; then
+			msg 'R' "* Il tool ${1}, necessario per l'esecuzione di questo script, non è presente nel sistema.\nInstallarlo per poter continuare."
+			tools_missing=true
+		fi
+		shift
+	done
+
+	[ "${tools_missing}" == true ] && return ${EXIT_FAILURE} || return ${EXIT_SUCCESS}
+}
+
+function read_filename_to_backup {
+	if ! [ -s "${config_file}" ]; then
+		msg 'R' "ERRORE: Il file \"${config_file}\" sembra non contenere informazioni utili."
+		return ${EXIT_FAILURE}
+    elif [ -f "${config_file}" ]; then
+    	local path
+        while IFS='' read -r path; do
+        	# bug: se legge riga del tipo ":/Volumes" considera chiave:"Volumes", value:"null"
+        	# invece dovrebbe considerare il contrario
+        	IFS=':'
+        	local tmp=(${path}) # crea un array considerando che gli elementi siano separati da IFS
+        	unset IFS
+        	[ -z "${tmp[0]}" ] && tmp[0]="${NULL}"
+        	[ -z "${tmp[1]}" ] && tmp[1]="${NULL}"
+
+    		source_dest_backup+=(["${tmp[0]}"]="${tmp[1]}")
+		done <<< `sed -e 's/[[:space:]]*\[.*]//; s/[[:space:]]*#.*//; /^[[:space:]]*$/d;' "${config_file}"`
+		unset IFS
+    else
+        msg 'R' "ERRORE: il file utilizzato per l'inizializzazione non è stato trovato! (PATH: ${config_file})"
+        return ${EXIT_FAILURE}
+    fi
+}
+
 function get_response {
 	msg "${1}" "${2}\t[ S / N ]"
 
+	local choose
 	read -e choose
 	if [ "${choose}" == "s" ] || [ "${choose}" == "S" ]; then
-		return $EXIT_SUCCESS
+		return ${EXIT_SUCCESS}
 	else
-		return $EXIT_FAILURE
+		return ${EXIT_FAILURE}
 	fi
 }
 
@@ -137,11 +165,13 @@ EOF
 }
 
 function parse_input {
-	while [ $# -gt 0 ]; do
-		case "$1" in
+	while [ ${#} -gt 0 ]; do
+		case "${1}" in
 			-a | --archive )
 				shift
-				tmp=(${1//:/ })
+				IFS=':'
+				local tmp=(${1})
+				unset IFS
 	        	[ -z "${tmp[0]}" ] && tmp[0]="${NULL}"
 	        	[ -z "${tmp[1]}" ] && tmp[1]="${NULL}"
 
@@ -198,7 +228,12 @@ function parse_input {
 }
 
 function sync_operation {
-	msg 'NC' "\nSincronizzazione direcotry \"${1}\" nella directory \"${2}\""
+	if [ "${1:(-1)}" == '/' ]; then
+		msg 'NC' "\nSincronizzazione CONTENUTI direcotry \"${1}\" nella directory \"${2}\""
+	else
+		msg 'NC' "\nSincronizzazione INTERA direcotry \"${1}\" nella directory \"${2}\""
+	fi
+
 	if [ "${ask}" == true ] && ! get_response 'Y' "Continuare?"; then
 		msg 'Y' "La sincronizzazione di \"${1}\" in \"${2}\" è stata interrotta"
 		return ${EXIT_FAILURE}
@@ -213,7 +248,7 @@ function sync_operation {
 }
 
 function archive {
-	msg 'NC' "\nArchiviazione directory \"${1}\" nella directory \"${2}\""
+	msg 'NC' "\nArchiviazione directory \"${1}\" nella directory \"${2}\"\nAl termine verrà eliminata la directory sorgente"
 	if [ "${ask}" == true ] && ! get_response 'Y' "Continuare?"; then
 		msg 'Y' "L'archiviazione di \"${1}\" in \"${2}\" è stata interrotta"
 		return ${EXIT_FAILURE}
@@ -230,13 +265,19 @@ function archive {
 function print_configuration {
 	msg 'BD' "##### Configurazione ${NC}- ${config_file}"
 
+	printf "${BD}* Directory files temporanei:${NC} ${tmp_dir}\n"
+
+    if [ "${log_on_file}" == true ] && [ -n "${log_file}" ]; then
+    	printf "${BD}* File di log:${NC} ${log_file}\n"
+    fi
+
 	if [ ${#source_dest_archive[@]} -eq 0 ]; then
 		msg 'BD' "* Filenames considerati per l'archiviazione: ${NC}${NULL}"
 	else
 		msg 'BD' "* Filenames considerati per l'archiviazione:"
 		msg 'BD' "\t\tSorgente\t\t\tDestinazione"
 		i=1
-		for k in ${!source_dest_archive[@]}; do
+		for k in "${!source_dest_archive[@]}"; do
 			printf "\t${BD}${i}.${NC} ${k} -> ${source_dest_archive["${k}"]}\n"
 			i=$((++i))
 		done
@@ -250,29 +291,23 @@ function print_configuration {
 		msg 'BD' "* Filenames considerati per il backup:"
 		msg 'BD' "\t\tSorgente\t\t\tDestinazione"
 		i=1
-		for k in ${!source_dest_backup[@]}; do
+		for k in "${!source_dest_backup[@]}"; do
 			printf "\t${BD}${i}.${NC} ${k} -> ${source_dest_backup["${k}"]}\n"
 			i=$((++i))
 		done
 	fi
 
-    printf "${BD}* Directory files temporanei:${NC} ${tmp_dir}\n"
-
-    if [ "${log_on_file}" == true ] && [ -n "${log_file}" ]; then
-    	printf "${BD}* File di log:${NC} ${log_file}\n"
-    fi
-
     msg 'BD' "#####\n"
 }
 
 function perform_backup {
-	for k in ${!source_dest_backup[@]}; do
+	for k in "${!source_dest_backup[@]}"; do
 		sync_operation "${k}" "${source_dest_backup["${k}"]}"
 	done
 }
 
 function perform_archive {
-	for k in ${!source_dest_archive[@]}; do
+	for k in "${!source_dest_archive[@]}"; do
 		archive "${k}" "${source_dest_archive["${k}"]}"
 	done
 }
@@ -282,14 +317,14 @@ function lazy_init_vars {
 }
 
 function validation_check {
-	file_error=false;
+	local file_error=false;
 
-	for k in ${!source_dest_backup[@]}; do
+	for k in "${!source_dest_backup[@]}"; do
 		! [ -d "${k}" ] && msg 'Y' "Attenzione: il file \"${k}\" non esiste." && file_error=true
 		! [ -d "${source_dest_backup["${k}"]}" ] && msg 'Y' "Attenzione: il file \"${source_dest_backup["${k}"]}\" non esiste." && file_error=true
 	done
 
-	for k in ${!source_dest_archive[@]}; do
+	for k in "${!source_dest_archive[@]}"; do
 		! [ -d "${k}" ] && msg 'Y' "Attenzione: il file \"${k}\" non esiste." && file_error=true
 		! [ -d "${source_dest_archive["${k}"]}" ] && msg 'Y' "Attenzione: il file \"${source_dest_archive["${k}"]}\" non esiste." && file_error=true
 	done
@@ -298,6 +333,9 @@ function validation_check {
 }
 
 function main {
+
+	check_tools rsync read printf [
+
 	parse_input "${@}" || return ${EXIT_FAILURE}
 
 	lazy_init_vars
@@ -316,7 +354,11 @@ function main {
 	perform_archive
 	perform_backup
 
+	# Flush buffers in scrittura
+	sync
+
 	return ${EXIT_SUCCESS}
+
 }
 
 main "${@}"
